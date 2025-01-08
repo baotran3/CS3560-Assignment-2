eec64b99ac1412f6007cea2b95b6b6487e1f6974
baotran3 <130107299+baotran3@users.noreply.github.com>
2025-01-08 04:33:10 -0800

    Create Transcript.java

---
+++ Question 5/a/Transcript.java
@@ -0,0 +1,9 @@
+class Transcript {
+    String course;
+    double grade;
+
+    public Transcript(String course, double grade) {
+        this.course = course;
+        this.grade = grade;
+    }
+}
