59c0cbeb95ce86df6f06a4b3dffd952d85ddcdd6
baotran3 <130107299+baotran3@users.noreply.github.com>
2025-01-08 04:33:40 -0800

    Delete Question 5/a directory

--- Question 5/a/Student2.java
+++
@@ -1,7 +0,0 @@
-class Student2 {
-    String name;
-
-    public Student2(String name) {
-        this.name = name;
-    }
-}
--- Question 5/a/Transcript.java
+++
@@ -1,9 +0,0 @@
-class Transcript {
-    String course;
-    double grade;
-
-    public Transcript(String course, double grade) {
-        this.course = course;
-        this.grade = grade;
-    }
-}
